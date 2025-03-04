5dfb8f4d97b93c6f4e40e090139f471ab25776b4
Buenaparte <131205534+Buenaparte@users.noreply.github.com>
2025-03-04 18:40:42 -0400

    version 0.1
    solo se realizo el iniciar seccion

---
+++ version 0.1 proyecto (luego vemos como lo organizamos)/src/iniciar_secion.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <>
+      <h1>iniciar secion</h1>
+      <form>
+        <label for= "username">Nombre de usuario</label><br></br>
+        <input type="text" id="username" name="username"></input><br></br>        
+        <label for= "contraseña">contraseña</label><br></br>
+        <input type="text" id="contraseña" name="contraseña"></input><br></br>
+        <label for= "g"></label><br></br>
+        <input type="submit" value="Submit"></input><br></br>
+      </form>
+      <p>iniciar seccion con google</p>
+      <p>iniciar seccion con facebook</p>
+    </>
+  )
+}
+
+export default App
+
+
